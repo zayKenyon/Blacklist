f3240cc440d85a58bb11f025719810e46234f66a
Isaac Kenyon <87788699+isaacKenyon@users.noreply.github.com>
2022-04-10 16:21:53 +0100

    command cleanup

--- commands/blacklist.js
+++ commands/blacklist.js
@@ -15,31 +15,27 @@ module.exports = {
                 .setRequired(true)
                 .setDescription('Enter a reason')),
     async execute(interaction, client) {
+        if (!interaction.member.permissions.has("MANAGE_GUILD")) return interaction.reply({ content: 'Lol no 🖕', ephemeral: true })
 
-        if (interaction.member.permissions.has("ADMINISTRATOR")) {
-            const user = interaction.options.getUser('target');
-            const string = interaction.options.getString('reason');
+        const user = interaction.options.getUser('target');
+        const string = interaction.options.getString('reason');
 
-            new UserSchema({
-                user: `${user.id}`,
-                reason: `${string}`,
-                guild: `${interaction.guild.id}`,
-                author: `${interaction.user.id}`,
-            }).save()
+        new UserSchema({
+            user: `${user.id}`,
+            reason: `${string}`,
+            guild: `${interaction.guild.id}`,
+            author: `${interaction.user.id}`,
+        }).save()
 
-            const Embed = new MessageEmbed()
-                .setTitle(`:loudspeaker: New Member Blacklisted!`)
-                .setColor("WHITE")
-                .setDescription(`User :: **${user}** \`${user.id}\`\nReason :: **${string}**\nAuthor :: **${interaction.user}** \`${interaction.user.id}\`\nGuild :: **${interaction.guild.name}**`)
-                .setThumbnail(`${user.displayAvatarURL()}`)
+        const Embed = new MessageEmbed()
+            .setTitle(`:loudspeaker: New Member Blacklisted!`)
+            .setColor("WHITE")
+            .setDescription(`User :: **${user}** \`${user.id}\`\nReason :: **${string}**\nAuthor :: **${interaction.user}** \`${interaction.user.id}\`\nGuild :: **${interaction.guild.name}**`)
+            .setThumbnail(`${user.displayAvatarURL()}`)
 
-            const channel = client.channels?.cache.get('832783717747130378') //An announcement channel all servers follow
-            channel.send({ embeds: [Embed] });
+        const channel = client.channels?.cache.get('832783717747130378') //An announcement channel all servers follow
+        channel.send({ embeds: [Embed] });
 
-            return interaction.reply({ embeds: [Embed]})
-
-        } else {
-            await interaction.reply({ content: 'Lol no 🖕', ephemeral: true })
-        }
+        interaction.reply({ embeds: [Embed]})
     }
 }
