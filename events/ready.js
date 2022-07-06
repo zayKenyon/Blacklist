6a131726780b9c1a9f72c7c84392240b9ff22db6
Zay Kenyon <87788699+isaacKenyon@users.noreply.github.com>
2022-07-06 23:08:04 +0100

    improved status

--- events/ready.js
+++ events/ready.js
@@ -3,10 +3,16 @@ module.exports = {
 	once: true,
 	execute(client) {
 		client.user.setStatus('dnd');
-		client.user.setActivity('y\'all 👀 | /help', { type: 'WATCHING' });
 		client.guilds.cache.forEach(guild => {
 			console.log(`${guild.name} (${guild.id})`);
 		});
+
+		const memberCount = client.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0);
+		const roundedMemberCount = Math.round(memberCount / 1000) * 1000;
+		const truncatedMemberCount = roundedMemberCount.toLocaleString('en');
+
+		const serverCount = client.guilds.cache.size;
+		client.user.setActivity(`${truncatedMemberCount} in ${serverCount} servers.`, { type: 'WATCHING' });
 
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 	},
