4d2d3cec7b2bc43ab0b0e5550ccc68152ab0383e
Zay Kenyon <isaac@kenyon.co.uk>
2022-07-22 20:37:26 -0400

    chore(deploycommands): bump REST version

--- deploy-commands.js
+++ deploy-commands.js
@@ -11,14 +11,27 @@ for (const file of commandFiles) {
 	commands.push(command.data.toJSON());
 }
 
-const rest = new REST({ version: '9' }).setToken(token);
+const rest = new REST({ version: '10' }).setToken(token);
 
-// Use this when testing
-// rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-// 	.then(() => console.log('Successfully registered application commands.'))
-// 	.catch(console.error);
+(async () => {
+	try {
+		console.log('Started refreshing application (/) commands.');
 
-// Use this when globally pushing commands
-rest.put(Routes.applicationCommands(clientId), { body: commands })
-	.then(() => console.log('Successfully registered application commands.'))
-	.catch(console.error);
+		// Use this when testing
+		// await rest.put(
+		// 	Routes.applicationGuildCommands(clientId, guildId),
+		// 	{ body: commands },
+		// );
+
+		// Use this when globally pushing commands
+		await rest.put(
+			Routes.applicationCommands(clientId),
+			{ body: commands },
+		);
+
+		console.log('Successfully reloaded application (/) commands.');
+	}
+	catch (error) {
+		console.error(error);
+	}
+})();
